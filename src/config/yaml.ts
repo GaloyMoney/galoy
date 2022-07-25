0c04ffdec06b939a6faef664bd42afeb099b6e5a
Juan P Lopez <dolcalmi@gmail.com>
2022-07-25 18:55:32 -0500

    refactor: rename dynamic fields to custom fields

--- src/config/yaml.ts
+++ src/config/yaml.ts
@@ -306,5 +306,5 @@ export const getRewardsConfig = () => {
 
 export const getAccountsConfig = (config = yamlConfig): AccountsConfig => ({
   initialStatus: config.accounts.initialStatus as AccountStatus,
-  information: config.accounts.information,
+  customFields: config.accounts.customFields,
 })
